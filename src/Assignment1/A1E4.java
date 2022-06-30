5a4ad5652a02b9f776191fa15763a83cf6aa60cf
Jan Balangue <jan.a.balangue@gmail.com>
2022-06-29 22:46:43 -0500

    Add Assigment2 exercises and rename Assignment1 exercises

---
+++ src/Assignment1/A1E4.java
@@ -0,0 +1,16 @@
+package Assignment1;
+
+public class A1E4 {
+	
+	public static void main(String[] args) {
+		
+		int year = 2022;
+		
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+			System.out.println(year + " is a leap year");
+		} else {
+			System.out.println(year + " is not a leap year");
+		}
+	}
+
+}
---
+++ src/Assignment2/A1E3.java
@@ -0,0 +1,30 @@
+package Assignment2;
+
+public class A1E3 {
+	
+	public static void main(String[] args) {
+		
+		int num = 13;
+		
+		System.out.println("This program determines if " + num + " is prime.");
+		System.out.println("It does this by iterating divisors from 2 to the square root of " + num + " and checking for a remainder.");
+		System.out.println("If a quotient without a remainder is not found by this point, " + num + " is prime.\n");
+		
+		if (isPrime(num)) {
+			System.out.println(num + " is prime.");
+		} else {
+			System.out.println(num + " is not prime.");
+		}
+	}
+	
+	public static boolean isPrime(int num) {
+		for (int i = 2; i < Math.sqrt(num); i++) {
+			if (num % i == 0 && num != i) {
+				System.out.println("Divisor is " + i);
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
